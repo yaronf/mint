993f5e223fb6bf245bc1f0c0930a4f69940f3aa6
Yaron Sheffer <yaronf.ietf@gmail.com>
2016-04-09 00:08:28 -0300

    Server gets name, private key and cert from flags

--- bin/mint-server/main.go
+++ bin/mint-server/main.go
@@ -6,16 +6,65 @@ import (
 	"net"
 
 	"../../../mint"
+	"crypto/x509"
+	"crypto/rsa"
+	"io/ioutil"
+	"encoding/pem"
 )
 
 var port string
+var serverName, serverKeyFile, serverCertFile string
+
+func readServerKey(serverKeyFile string) *rsa.PrivateKey {
+	serverKeyBytes, err := ioutil.ReadFile(serverKeyFile)
+	if err != nil {
+		log.Fatalf("Cannot read key: %s", serverKeyFile)
+	}
+	serverKeyPEM, _ := pem.Decode(serverKeyBytes)
+	serverKeyDER := serverKeyPEM.Bytes
+	serverKey, err    := x509.ParsePKCS1PrivateKey(serverKeyDER)
+	if err != nil {
+		log.Fatalf("Cannot parse private key: %s", serverKeyFile)
+	}
+	return serverKey
+}
+
+func readServerCert(serverCertFile string) *x509.Certificate {
+	serverCertBytes, err := ioutil.ReadFile(serverCertFile)
+	if err != nil {
+		log.Fatalf("Cannot read cert: %s", serverCertFile)
+	}
+	serverCertPEM, _ := pem.Decode(serverCertBytes)
+	serverCertDER := serverCertPEM.Bytes
+	serverCert, err := x509.ParseCertificate(serverCertDER)
+	if err != nil {
+		log.Fatalf("Cannot parse cert: %s", serverCertFile)
+	}
+	return serverCert
+}
 
 func main() {
 	var config mint.Config
-	config.Init(false)
 
 	flag.StringVar(&port, "port", "4430", "port")
+	flag.StringVar(&serverName, "servername", "", "server name")
+	flag.StringVar(&serverKeyFile, "keyfile", "", "private key file")
+	flag.StringVar(&serverCertFile, "certfile", "", "certificate file")
 	flag.Parse()
+
+	serverKey := readServerKey(serverKeyFile)
+	serverCert := readServerCert(serverCertFile)
+
+	certificates := []*mint.Certificate{
+		&mint.Certificate{
+			Chain:      []*x509.Certificate{serverCert},
+			PrivateKey: serverKey,
+		},
+	}
+	config = mint.Config{
+		ServerName: serverName,
+		Certificates: certificates,
+	}
 
 	service := "0.0.0.0:" + port
 	listener, err := mint.Listen("tcp", service, &config)
